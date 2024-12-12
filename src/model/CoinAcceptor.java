6145fd36e3af69e8cce63c220c8c821958388b01
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-13 00:21:46 +0600

    Changed the CoinAcceptor class to the interface

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,24 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor {
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
-    public int getAmount() {
+    @Override
+    public void acceptPayment(int amount) {
+        this.amount += amount;
+    }
+
+    @Override
+    public int getBalance() {
         return amount;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void resetBalance() {
+        this.amount = 0;
     }
 }
